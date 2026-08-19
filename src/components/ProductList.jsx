0142801b4a4451604ee2e90754b8331a133f1a9a
kalaumapathy <50033847+kalaumapathy@users.noreply.github.com>
2026-08-18 19:42:08 -0500

    create product list component

---
+++ src/components/ProductList.jsx
@@ -0,0 +1,16 @@
+import ProductCard from "./ProductCard";
+
+function ProductList({ products }) {
+  return (
+    <div>
+      {products.map((product) => (
+        <ProductCard
+          key={product.id}
+          product={product}
+        />
+      ))}
+    </div>
+  );
+}
+
+export default ProductList;
